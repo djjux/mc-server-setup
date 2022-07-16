5d8fb76c90611ec3725516403c9ec299aac833e0
jose <pintovillamar@outlook.com>
2022-07-15 19:50:06 -0500

    upgrade

--- start.bat
+++ start.bat
@@ -1,1 +1,8 @@
-java -Xmx1024M -Xms1024M -jar server.jar nogui
+set /A MAXRAM=1024
+set /A MINRAM=1024
+
+:start
+java -Xmx%MAXRAM%M -Xms%MINRAM%M -jar server.jar nogui
+timeout 10
+echo Restarting server...
+goto start
