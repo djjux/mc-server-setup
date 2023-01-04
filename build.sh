82453cebb96b9109f2181580612ad71ed2132dae
jose pinto <pintovillamar@outlook.com>
2023-01-03 23:56:08 -0500

    build.sh added to actually build the script

---
+++ build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Set executable permissions for all .sh files in the repository
+echo "Setting executable permissions for script files..."
+find . -name "*.sh" -exec chmod +x {} \;
+
+# Print a fake progress bar
+echo -n "Progress: ["
+for i in {1..50}; do
+    sleep 0.1
+    echo -n "#"
+done
+echo "]"
+
+# Run the build process
+# Replace this line with your actual build process commands
+echo "Build process complete."
